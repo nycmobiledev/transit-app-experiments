977ef24eb1ee6f1544bd22511c2700b560e8c7e2
Vincent Bortone <vbortone@gmail.com>
2014-05-09 18:29:19 -0400

    Initial Project Setup

---
+++ nearest-station-webapi/nearest-station-gtfs/Domain.Interfaces/IStationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYCMobileDev.TransitApp.Domain.Interfaces
+{
+    public interface IStationRepository
+    {
+    }
+}
