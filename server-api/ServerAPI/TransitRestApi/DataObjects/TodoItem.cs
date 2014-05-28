d6c5e1de6921c2a22d8a733070cb6d207f811323
Vincent Bortone <vbortone@gmail.com>
2014-05-28 01:16:35 -0400

    Server API Project Setup

---
+++ server-api/ServerAPI/TransitRestApi/DataObjects/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace NYCMobileDev.TransitApp.TransitRestApi.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
---
+++ server-api/ServerAPI/TransitRestApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TransitRestApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NYC .Net Mobile Dev Meetup")]
+[assembly: AssemblyProduct("NYC Transit App")]
+[assembly: AssemblyCopyright("Copyright ©  2014 NYC .Net Mobile Dev Meetup")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f19bb411-c88b-47d7-be2d-def684158b00")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
