b6fed7844208a79ebc536688ce0f0b565dae42cf
Vincent Bortone <vbortone@gmail.com>
2014-05-28 02:09:19 -0400

    Create Service Interfaces and Domain Entities

---
+++ server-api/ServerAPI/Domain.Entities/TransitOption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NYCMobileDev.TransitApp.Domain.Entities
+{
+    public class TransitOption
+    {
+        public string Name { get; set; }
+        public string IconUrl { get; set; }
+        public string Destination { get; set; }
+        public double MinutesAway { get; set; }
+    }
+}
