2f4b595e2966630d2c1d9393640400dcb6f1dd86
Vincent Bortone <vbortone@gmail.com>
2014-06-02 10:47:16 -0400

    Add realtime project to vfb folder

---
+++ vfb/MTARealtimeTestProjects/OsmSharpTestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTFS;
+using GTFS.IO;
+
+namespace OsmSharpTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new GTFSReader<GTFSFeed>();
+
+            // execute the reader.
+            var feed = reader.Read(new GTFSDirectorySource(new DirectoryInfo("c:\\temp\\GTFS Static")));
+
+            var trip = feed.GetTrip("A20131215SAT_000250_2");
+            Console.WriteLine(trip.ToString());
+        }
+    }
+}
