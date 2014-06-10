68202eba57ebbcca23d03b1e6eb2aaa761058eb9
Vincent Bortone <vbortone@gmail.com>
2014-06-10 11:34:06 -0400

    Implement GTFS project and start static code.

---
+++ gtfs/GTFSApi/Domain.GTFS.Static.Subway.Entities/Stop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NYCMobileDev.TransitApp.Domain.GTFS.Static.Subway.Entities
+{
+    public class Stop
+    {
+        public string StopId { get; set; }
+        public string StopName { get; set; }
+        public double StopLat { get; set; }
+        public double StopLng { get; set; }
+        public string LocationType { get; set; }
+        public string ParentStationId { get; set; }
+    }
+}
---
+++ gtfs/GTFSApi/Infrastructure.GTFS.Static.Subway.FileSystem/StopRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using NYCMobileDev.TransitApp.Domain.GTFS.Static.Subway.Entities;
+using NYCMobileDev.TransitApp.Domain.GTFS.Static.Subway.Interfaces;
+
+namespace NYCMobileDev.TransitApp.Infrastructure.GTFS.Static.Subway.FileSystem
+{
+    public class StopRepository : IStopRepository
+    {
+        private readonly string _stopFilePath;
+
+        public StopRepository(string stopFilePath)
+        {
+            _stopFilePath = stopFilePath;
+        }
+
+        public IEnumerable<Stop> GetAllStops()
+        {
+            var csv = new CsvReader(File.Op)
+        }
+    }
+}
