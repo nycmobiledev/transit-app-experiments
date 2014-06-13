a757a8af0829c23d89d9e6c3b94eee73bdef09a1
Vincent Bortone <vbortone@gmail.com>
2014-06-13 18:47:17 -0400

    Add Trip, Route and Stop Repository Tests

---
+++ gtfs/GTFSApi/Tests.GTFS.Static.Subway.IntegrationTests/RouteRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using NYCMobileDev.TransitApp.Infrastructure.GTFS.Static.Subway.FileSystem;
+
+namespace NYCMobileDev.TransitApp.Tests.GTFS.Static.Subway.IntegrationTests
+{
+    [TestFixture]
+    public class RouteRepositoryTests
+    {
+        private const int DataFileRowCountMinusHeader = 28;
+        private const string DataFilePath = "routes.txt";
+
+        [Test]
+        public void Should_Return_All_Routes()
+        {
+            using (var repos = new RouteRepository(DataFilePath)) {
+                var result = repos.GetAllRoutes();
+                Assert.That(result.Count(t => t != null), Is.EqualTo(DataFileRowCountMinusHeader));
+            }
+        }
+    }
+}
---
+++ gtfs/GTFSApi/Tests.GTFS.Static.Subway.IntegrationTests/TripRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NYCMobileDev.TransitApp.Infrastructure.GTFS.Static.Subway.FileSystem;
+
+namespace NYCMobileDev.TransitApp.Tests.GTFS.Static.Subway.IntegrationTests
+{
+    [TestFixture]
+    class TripRepositoryTests
+    {
+        private const int DataFileRowCountMinusHeader = 21193;
+        private const string DataFilePath = "trips.txt";
+
+        [Test]
+        public void Should_Return_All_Trips()
+        {
+            using (var repos = new TripRepository(DataFilePath)) {
+                var result = repos.GetAllTrips();
+                Assert.That(result.Count(t => t!=null), Is.EqualTo(DataFileRowCountMinusHeader));
+            }
+        }
+    }
+}
